49fda90fdce5b94b8db02e85c8256cfa1cc1a4b9
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-01-13 22:58:05 +0530

    updated groovy function with parameters

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
         sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
-        sh 'docker build -t ankitraz/myapp:ank-1.0 .'
-        sh 'docker push ankitraz/myapp:ank-1.0'
+        sh "docker build -t $imageName ."
+        sh "docker push $imageName"
     }
 }
