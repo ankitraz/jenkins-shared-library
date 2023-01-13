4a59297e32642801636b064b26e164b0066c024b
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-01-13 22:40:21 +0530

    updated crededtials

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 
 def call(){
     echo "Building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'gitlab-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
         sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
         sh 'docker build -t ankitraz/myapp:ank-1.0 .'
         sh 'docker push ankitraz/myapp:ank-1.0'
