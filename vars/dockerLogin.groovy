39707976ce2ec056e615117dc0e2f403ced08f27
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-01-16 20:30:18 +0530

    updated docker.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 import com.example.Docker
 def call(){
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
-    }
+    // withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    //     sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
+    // }
+
+    return new Docker(this).dockerLogin()
 }
