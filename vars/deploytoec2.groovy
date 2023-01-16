cbdc5987429299ea7775373942ddc42936ae3d50
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-01-16 21:38:16 +0530

    updated shared lib

--- vars/deploytoec2.groovy
+++ vars/deploytoec2.groovy
@@ -3,6 +3,6 @@
 def call(){
     def dockerCmd = 'docker run -d -p 8080:8080 ankitraz/myapp:1:0'
     sshagent(['ec2-server-key']){
-        sh 'ssh -o StrictHostkeyChecking=no ec2-user@43.204.211.220 ${dockerCmd}'
+        sh "ssh -o StrictHostkeyChecking=no ec2-user@43.204.211.220 ${dockerCmd}"
     }
 }
